8c8a0b8e1ac59f91e25eb64ab2f4f0b3114a860f
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-30 10:36:54 +0100

    More examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include <vector>
 
+#define DLL_SVM_SUPPORT
+
 #include "dll/rbm.hpp"
+#include "dll/dbn.hpp"
 #include "dll/ocv_visualizer.hpp"
 
+
+void read_data(std::vector<std::vector<double>>& samples, std::vector<std::size_t>& labels);
+
+void rbm_features();
+void svm_classify();
+
 int main(){
+    //rbm_features(); //Only one RBM, no classification, pure feature extraction
+    //svm_classify(); //Classify RBM features with SVM
+
+    return 0;
+}
+
+void rbm_features(){
     //1. Configure and create the RBM
 
     using rbm_t = dll::rbm_desc<
-       28 * 28,                       //Number of visible units
-       200                            //Number of hidden units
-       , dll::momentum                //Activate momentum ?
-       , dll::batch_size<25>          //Minibatch
-       , dll::weight_decay<>          //Activate weight decay ?
-       //, dll::sparsity<>              //Activate weight decay ?
-       //, dll::visible<dll::unit_type::GAUSSIAN>  //Gaussian visible units ?
-       //, dll::watcher<dll::opencv_rbm_visualizer>  //OpenCV Visualizer ?
+       28 * 28,                                             //Number of visible units
+       200                                                  //Number of hidden units
+       , dll::momentum                                      //Activate momentum ?
+       , dll::batch_size<25>                                //Minibatch
+       , dll::weight_decay<>                                //Activate weight decay ?
+       //, dll::sparsity<>                                  //Activate weight decay ?
+       //, dll::visible<dll::unit_type::GAUSSIAN>           //Gaussian visible units ?
+       //, dll::watcher<dll::opencv_rbm_visualizer>         //OpenCV Visualizer ?
     >::rbm_t;
 
     auto rbm = std::make_unique<rbm_t>();
@@ -24,8 +40,7 @@ int main(){
     std::vector<std::vector<double>> samples;     //All the samples
     std::vector<std::size_t> labels;              //All the labels
 
-    //TODO Read samples
-    //TODO Read labels
+    read_data(samples, labels);
 
     //3. Train the RBM for x epochs
 
@@ -34,4 +49,45 @@ int main(){
     //4. Get the activation probabilities for a sample
 
     auto probs = rbm->activation_probabilities(samples[0]);
+}
+
+void svm_classify(){
+    //1. Configure and create the RBM
+
+    using dbn_t = dll::dbn_desc<
+        dll::dbn_label_layers<
+            dll::rbm_desc<28 * 28, 100, dll::batch_size<50>, dll::init_weights, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t,
+            dll::rbm_desc<100, 100, dll::batch_size<50>, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t,
+            dll::rbm_desc<100, 200, dll::batch_size<50>, dll::momentum, dll::weight_decay<dll::decay_type::L2>>::rbm_t
+        >>::dbn_t;
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    //2. Read dataset
+
+    std::vector<std::vector<double>> samples;     //All the samples
+    std::vector<std::size_t> labels;              //All the labels
+
+    read_data(samples, labels);
+
+    //3. Train the DBN layers for x epochs
+
+    dbn->pretrain(samples, 100);
+
+    //4. Get the activation probabilities for a sample
+
+    auto probs = dbn->activation_probabilities(samples[0]);
+
+    //5. Train the SVM
+
+    dbn->svm_train(samples, labels);
+
+    //6. Predict the label of a sample
+
+    auto label = dbn->svm_predict(samples[0]);
+}
+
+void read_data(std::vector<std::vector<double>>& samples, std::vector<std::size_t>& labels){
+    //TODO Read samples
+    //TODO Read labels
 }
